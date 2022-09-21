b3233ad537bff3cc8a532496be62c4e68378c692
Hatice Kostekci <sbahar647@gmail.com>
2022-09-21 09:19:40 +0300

    v1

---
+++ src/BizimCalisma/C01_GithubCalisma.java
@@ -0,0 +1,17 @@
+package BizimCalisma;
+
+public class C01_GithubCalisma {
+    public static void main(String[] args) {
+            /*
+    1- git init : local de repo olusturmak
+    2- git add. : working space'den staging area'ya dosyalari gönderir. En son değişiklikleri,
+    yapılan tüm işlemleri gönderir.
+    3- git status : ne var ne yok? staging area'daki son durumu bize getirir.
+    4- git log : versiyonları bize gösterir.
+    5- git log--oneline : özet bilgi getirir.
+    6- git commit : versiyonlara dosyaları gönderir.
+     */
+
+    }
+
+}
